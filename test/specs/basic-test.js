e798db6aa5fde5031ad5aa172e20bc8c006ec18a
Valentin Vago <zeropaper@irata.ch>
2017-08-22 11:38:00 +0200

    update wdio test

--- test/specs/basic-test.js
+++ test/specs/basic-test.js
@@ -1,12 +1,17 @@
 var assert = require('assert');
+
 describe('Loader Simulation', function() {
   before(function() {
     browser.url('http://localhost:8080');
   });
 
-  it('has at least', function() {
+  it('has at least 2 loaders', function() {
     var progressElements = browser.elements('.progress');
-    console.info('progressElements', progressElements);
-    assert.equal(progressElements.value.length, 2);
+    assert(progressElements.value.length >= 2);
+  });
+
+  it('has at least 2 buttons', function() {
+    var buttonElements = browser.elements('button');
+    assert(buttonElements.value.length >= 2);
   });
 });
